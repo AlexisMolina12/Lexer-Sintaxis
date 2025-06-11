ca419f711948b8e963f446d7fa39aa9b961e557f
AlexisMolina12 <ale.18.2002.moli@gmail.com>
2025-06-11 20:38:33 -0300

    empezamos el lexer

---
+++ Lexer.py
@@ -0,0 +1,31 @@
+#Importamos los automatas
+from Automatas.and import *
+from Automatas.begin import *
+from Automatas.bool import *
+from Automatas.dos_puntos_igual import *
+from Automatas.else import *
+from Automatas.end import *
+from Automatas.false import *
+from Automatas.id import *
+from Automatas.if import *
+from Automatas.igual import *
+from Automatas.int import *
+from Automatas.multiplicacion import *
+from Automatas.not import *
+from Automatas.num import *
+from Automatas.or import *
+from Automatas.parentesisA import *
+from Automatas.parentesisB import *
+from Automatas.punto_y_coma import *
+from Automatas.punto import *
+from Automatas.suma_resta import *
+from Automatas.trespuntos import *
+from Automatas.true import *
+from Automatas.var import *
+
+ESTADO_FINAL = "ESTADO FINAL"
+ESTADO_NO_FINAL = "NO ACEPTADO"
+ESTADO_TRAMPA = "EN ESTADO TRAMPA"
+ESTADOS_POSIBLES 
+
+
